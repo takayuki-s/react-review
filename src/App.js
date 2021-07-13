ac03a834980629eb28e103186d5b683e9de397c2
takayuki-s <taka12yuki04@gmail.com>
2021-07-13 09:26:26 +0900

    コンポーネントレンダリング復習

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { ChildArea } from './ChildArea';
 
 function App() {
-  console.log("App")
-  const [count, setCount] = useState(0);
-  const onClickCountUp = () =>{
-    setCount(count + 1);
-  }
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+  const onChangeText = (e) => setText(e.target.value);
+  
+  const onClickOpen = (e) => setOpen(!open)
+  // const onClickCountUp = () =>{
+  //   setCount(count + 1);
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{count}</p>
-        <button onClick={onClickCountUp}>カウントアップ</button>
-      </header>
+      <br />
+      <input value={text} onChange={onChangeText}/>
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
     </div>
   );
 }
