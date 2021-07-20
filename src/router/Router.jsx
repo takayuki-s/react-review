ce97ca5190df84d40017b3002d05ee095c93bfc7
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-21 06:16:40 +0900

    page2ルーティングネスト、コンポーネント分割

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,36 +2,35 @@ import { Switch, Route } from 'react-router-dom';
 import { Home } from '../Home';
 import { Page2 } from '../Page2';
 import { page1Routes } from './Page1Routes';
+import { page2Routes } from './Page2Routes';
 
 
 export const Router = () => {
   return (
     <Switch>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route path="/page1" render={({ match: { url }}) => (
-      <Switch>
-        {/* <Route exact path={url}>
-          <Page1 />
-        </Route>
-        <Route path={`${url}/detailA`}>
-          <Page1DetailA />
-        </Route>
-        <Route path={`${url}/detailB`}>
-          <Page1DetailB />
-        </Route> */}
-        {page1Routes.map((route) => (
-          <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
-            {route.children}
-          </Route>
-        ))}
-      </Switch>
-    )}>
-    </Route>
-    <Route path="/page2">
-      <Page2 />
-    </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/page1" render={({ match: { url }}) => (
+        <Switch>
+          {page1Routes.map((route) => (
+            <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
+      )}>
+      </Route>
+      <Route path="/page2" render={({ match: { url }}) => (
+        <Switch>
+          {page2Routes.map((route) => (
+            <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
+      )}>
+      </Route>
     </Switch>
 
   )
