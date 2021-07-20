ea5223f093038afa0484dac1b77d76e26b971323
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-20 09:02:50 +0900

    ルーティング切り分け

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import './App.css';
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page1DetailB } from './Page1Detail2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page2 } from './Page2';
+import { Router } from './router/Router';
 
 function App() {
 
@@ -18,29 +14,7 @@ function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page1" render={({ match: { url }}) => (
-          <Switch>
-            {console.log(url)}
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
-          </Switch>
-        )}>
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
